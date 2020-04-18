0da7dded38a156aa57943a99cf1c90ff5acf6d71
Carlos McNulty <cmcnul3@uic.edu>
2020-04-17 22:11:51 -0500

    init commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "graphql"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.7.0-M2"
+libraryDependencies += "org.wso2.apache.httpcomponents" % "httpclient" % "4.3.1.wso2v1"
+libraryDependencies += "commons-logging" % "commons-logging" % "1.2"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,38 @@
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
+import org.json4s.DefaultFormats
+import org.json4s.native.Document
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+import scala.io.Source.fromInputStream
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    val BASE_GHQL_URL = "https://api.github.com/graphql"
+    val temp="{viewer {email login url}}"
+    implicit val formats = DefaultFormats
+
+    val client = HttpClientBuilder.create().build()
+    val httpUriRequest = new HttpPost(BASE_GHQL_URL)
+    httpUriRequest.addHeader("Authorization", "bearer a972582212ffa4468f99c0d1642703b480f4c892")
+    httpUriRequest.addHeader("Accept", "application/json")
+    val gqlReq = new StringEntity("{\"query\":\"" + temp + "\"}" )
+    httpUriRequest.setEntity(gqlReq)
+
+    val response = client.execute(httpUriRequest)
+    System.out.println("Response:" + response)
+    response.getEntity match {
+      case null => System.out.println("Response entity is null")
+      case x if x != null => {
+        val respJson = fromInputStream(x.getContent).getLines.mkString
+        System.out.println(respJson)
+        //val viewer = parse(respJson).extract[Data]
+        //System.out.println(viewer)
+        //System.out.println(write(viewer))
+      }
+    }
+  }
+}
