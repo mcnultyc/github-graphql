b1a01c8b29490d741fe7706f38c03f4f1ac7bccb
Carlos McNulty <cmcnul3@uic.edu>
2020-05-02 19:00:50 -0500

    added github exception throws to execute

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -4,6 +4,11 @@ import java.io.{ByteArrayOutputStream, PrintStream, OutputStream}
 import com.typesafe.config.ConfigFactory
 import org.slf4j.{Logger, LoggerFactory}
 
+import org.json4s.DefaultFormats
+import org.json4s._
+import org.json4s.jackson.JsonMethods.{parse, _}
+
+
 
 object Driver {
 
@@ -53,16 +58,26 @@ object Driver {
     val allLangs = LanguageInfo.values.toList
     val allRepo = RepoInfo.values.toList
 
+    try {
 
-    val query: QueryCommand = QueryBuilder()
-      .withRepos(allRepo)
-      .withAuth(github)
-      .withStarGazers(allUsers)
-      .withCollaborators(allUsers)
-      .withCommits(allCommits)
-      .withIssues(allIssues)
-      .withLanguages(allLangs)
-      .build
+      val query: QueryCommand = QueryBuilder()
+        .withRepos(allRepo)
+        .withAuth(github)
+        .withStarGazers(allUsers)
+        .withCollaborators(allUsers)
+        .withCommits(allCommits)
+        .withIssues(allIssues)
+        .withLanguages(allLangs)
+        .build
+    }
+    catch{
+      case e: GitHubConnectionException => {
+        System.err.println("Status: " + e.status)
+        System.err.println("Message: " + e.message)
+        e.printStackTrace()
+
+      }
+    }
 
   }
 }
