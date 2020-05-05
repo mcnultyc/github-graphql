6fbdbc559ddea7f7062f47b1aabd59919039073f
Carlos McNulty <cmcnul3@uic.edu>
2020-05-05 18:55:06 -0500

    added example that should work for anyone

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -10,8 +10,9 @@ object Driver {
   val conf = ConfigFactory.load().getConfig("GQL")
   val logger = LoggerFactory.getLogger(Driver.getClass)
 
-
   def printData(repos: List[(String, Map[String, Any])], max: Int = 3): Unit ={
+
+    logger.info("printing filtered data")
 
     println("\nFILTERED DATA: ")
     println("-------------------------------------")
@@ -26,7 +27,6 @@ object Driver {
   }
 
 
-
   def main(args: Array[String]): Unit = {
 
 
@@ -38,19 +38,6 @@ object Driver {
       .withAuth(TOKEN)
       .withHeaders(List((ACCEPT, APP_JSON)))
       .build
-
-/*
-    val query: QueryCommand = QueryBuilder()
-      .withRepoOwner("linux", "torvalds")
-      .withAuth(github)
-      .withStarGazers(List(UserInfo.NAME, UserInfo.EMAIL))
-      .withCollaborators(List(UserInfo.NAME, UserInfo.EMAIL))
-      .withCommits(List(CommitInfo.AUTHOR))
-      .withIssues(List(IssueInfo.AUTHOR))
-      .withLanguages(List(LanguageInfo.NAME))
-      .build
-
- */
 
     try {
 
@@ -64,8 +51,8 @@ object Driver {
         .withLanguages(List(LanguageInfo.NAME))
         .build
 
-
-      printData(query.filter(Commit(CommitInfo.TOTAL_COUNT, (x) => x == "40")))
+      // Filter repos were the primary language is Java
+      printData(query.filter(Languages(LanguageInfo.NAME, (x) => x == "Java")))
     }
     catch{
       // Check for github connection exception and graphql errors
@@ -78,50 +65,6 @@ object Driver {
 
       }
     }
-
-
-
-
-/*
-    val allIssues = IssueInfo.values.toList
-    val allCommits = CommitInfo.values.toList
-    val allUsers = UserInfo.values.toList
-    val allLangs = LanguageInfo.values.toList
-    val allRepo = RepoInfo.values.toList
-
-    try {
-
-      val query: QueryCommand = QueryBuilder()
-        .withRepos(allRepo)
-        .withAuth(github)
-        .withStarGazers(allUsers)
-        .withCollaborators(allUsers)
-        .withCommits(allCommits)
-        .withIssues(allIssues)
-        .withLanguages(allLangs)
-        .build
-
-      query.filter(CommitInfo.AUTHOR, (x: String) => {
-        if(x == "mcnultyc"){
-          true
-        }
-        else{
-          false
-        }
-      })
-
-    }
-    catch{
-      case e: GitHubConnectionException => {
-        System.err.println("Status: " + e.status)
-        System.err.println("Message: " + e.message)
-        e.printStackTrace()
-
-      }
-    }
-
-
- */
 
   }
 
